1c2720fce0fe97d3d0a26635da8e0e77901d5a72
Luke Berger <berge156@mail.chapman.edu>
2019-09-16 20:08:47 -0700

    This is my cookie Clicker version 1.12!

--- app/src/main/java/com/example/bigstupiddaddy/MainActivity.kt
+++ app/src/main/java/com/example/bigstupiddaddy/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.example.bigstupiddaddy
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import com.example.bigstupiddaddy.util.toggleVisibility
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var cookieCounter: Int = 0
+    private var cookieCounter: Long = 0
+    private val store = getPreferences(Context.MODE_PRIVATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState != null){
-            cookieCounter = savedInstanceState.getInt(COOKIE_COUNTER_KEY,0)
+            Log.d("TEST", "recreating saved state from $savedInstanceState")
+            cookieCounter = savedInstanceState.getLong(COOKIE_COUNTER_KEY,0)
         }
 
         myButton.setOnClickListener {
@@ -26,14 +30,21 @@ class MainActivity : AppCompatActivity() {
             cookieCounter++
             brandonImage.toggleVisibility()
             cookie_score.text = cookieCounter.toString()
+
         }
 
 
         }
+
+    override fun onPause() {
+        super.onPause()
+        store.edit().putLong(COOKIE_COUNTER_KEY, cookieCounter).commit()
+    }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         outState?.run{
-            putInt(COOKIE_COUNTER_KEY, cookieCounter)
+            Log.d("TEST", "got to onSaveInstanceState")
+            putLong(COOKIE_COUNTER_KEY, cookieCounter)
         }
 
         super.onSaveInstanceState(outState, outPersistentState)
